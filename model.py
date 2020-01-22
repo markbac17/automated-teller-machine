8af65da784c972179e5ef18c5658d044e9df42d1
Marek Bachurski <mark.bachurski@me.com>
2020-01-22 11:23:57 -0500

    added classes

--- model.py
+++ model.py
@@ -1,4 +1,53 @@
 import json
+import os
+
+DIR = os.path.dirname(__file__)
+DATAFILENAME = "accounts.json"
+DATAPATH = os.path.join(DIR, DATAFILENAME)
+
+class Account:
+
+    data_path = DATAPATH
+    
+    def __init__(self, account_num = "", user_name = "", pin_code = "", balance = 0)
+        """populate ...self.atributes for a bank account"""
+        self.account_num = account_num
+        self.user_name = user_name
+        self.pin_code = pin_code
+
+    def deposit(self):
+        """add a new float amount to self.balance"""
+        data = load()
+        account_balance = data[by_user_account]["balance"]
+        data[str(by_user_account)]["balance"] = int(account_balance) + int(add_amount)
+        save(data)
+
+    def withdraw(self):
+        """subtract a float amount from self.balance"""
+        data = load()
+        account_balance = data[by_user_account]["balance"]
+        data[str(by_user_account)]["balance"] = int(account_balance) - int(subtract_amount)
+        save(data)
+
+    def load():
+        """read our data file into a Python dictionary"""
+        with open(DATAPATH, 'r') as f_object:
+            data = json.load(f_object)
+        return data
+
+    def save(self):
+        """load our data and save ...self.attributes to our JSON file"""
+        with open('atm.json', 'w') as f_object:
+            json.dump(data, f_object, indent=2)
+
+    @classmethod
+    def validate(cls, account_num, pin):
+        """take an account number and pin as inputs and return the user info
+        from "data.json" if it exists, otherwise returns None
+        """
+        pass
+
+
 
 def create_new_account():
     data = load()
@@ -20,7 +69,6 @@ def add_pin_code(by_account_num, new_pin_code):
 def get_balance(by_account_num):
     data = load()
     account_balance = data[by_account_num]["balance"]
-    # print(account_balance)
     return account_balance
 
 def add_funds(by_user_account, add_amount):
